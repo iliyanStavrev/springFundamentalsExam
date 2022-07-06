e580cca7126950e0e225513f933ae61e7c004f6f
Iliyan <iliyanstavrev11@gmail.com>
2022-07-06 11:51:50 +0300

    Initial commit

---
+++ src/main/java/com/example/exam/init/Test.java
@@ -0,0 +1,12 @@
+package com.example.exam.init;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Test implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("This class is just for test for commits in github!");
+    }
+}
